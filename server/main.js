14025787ab0ef9c2adb849f4e1ec762545296b22
Cassius P <287ilias@gmail.com>
2021-03-18 14:39:54 +0100

    Display Films

--- server/main.js
+++ server/main.js
@@ -4,6 +4,10 @@ import { localDatas } from './local-datas.js';
 
 Meteor.startup(() => {});
 WebApp.connectHandlers.use('/api/discover/movie', (req, res, next) => {
-    res.writeHead(200);
-    res.end(JSON.stringify(localDatas));
+
+
+    const result = HTTP.call('GET', 'https://api.themoviedb.org/3/discover/movie?api_key=8cce820f4a3f0e0254ff5512352682fe&language=fr-FR', {});
+
+
+    res.end(JSON.stringify(result))
 });
