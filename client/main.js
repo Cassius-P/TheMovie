8c2a2f866d3c6cb8fdb9d62b319997aa03d0ec5c
Cassius P <287ilias@gmail.com>
2021-03-19 01:13:30 +0100

    Like system based on vote count

--- client/main.js
+++ client/main.js
@@ -2,6 +2,8 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import './script'
+
 
 Template.home.onCreated(function homeOnCreated() {
   let ctrl = this;
@@ -9,8 +11,9 @@ Template.home.onCreated(function homeOnCreated() {
   HTTP.call('GET', 'http://localhost:3000/api/discover/movie', {},
       function(error, response) {
         // Handle the error or response here.
-          ctrl.movies.set(JSON.parse(JSON.parse(response.content).content).results);
-          console.log(JSON.parse(JSON.parse(response.content).content).results)
+          console.log(JSON.parse(response.content))
+          ctrl.movies.set(JSON.parse(response.content));
+
       });
 });
 Template.home.helpers({
@@ -18,3 +21,29 @@ Template.home.helpers({
     return Template.instance().movies.get();
   }
 });
+
+Template.home.events({
+    "click .likeButton": (elmt)=>{
+        let button = elmt.target
+        likeFilm(button)
+    }
+});
+
+
+
+function likeFilm(button){
+
+    let id = button.dataset.id
+    console.log(id)
+    HTTP.call('POST', "http://localhost:3000/api/like/"+id,{}, (error, response)=>{
+        console.log("fn",response.content)
+        console.log(button.children)
+        if(response.content == "liked"){
+            button.children[0] = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill h-100 " viewBox="0 0 16 16">'+
+                                        '<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>'+
+                                    '</svg>'
+        }
+    });
+}
+
+
--- server/main.js
+++ server/main.js
@@ -1,13 +1,65 @@
 import { Meteor } from 'meteor/meteor';
+import crypto from 'crypto'
+import connectRoute from 'connect-route'
+
 
 import { localDatas } from './local-datas.js';
+const Likes = new Mongo.Collection('likes')
 
 Meteor.startup(() => {});
 WebApp.connectHandlers.use('/api/discover/movie', (req, res, next) => {
 
+    let result = HTTP.call('GET', 'https://api.themoviedb.org/3/discover/movie?api_key=8cce820f4a3f0e0254ff5512352682fe&language=fr-FR', {});
 
-    const result = HTTP.call('GET', 'https://api.themoviedb.org/3/discover/movie?api_key=8cce820f4a3f0e0254ff5512352682fe&language=fr-FR', {});
+    let films = JSON.parse(result.content).results
+    /*console.log(films)*/
+    for (let i = 0; i < films.length; i++) {
+        console.log(films[i].title , Likes.find({idFilm: films[i].id.toString()}).count() != 0 ? 'was liked' : "wasn't liked")
+        if (Likes.find({idFilm: films[i].id.toString()}).count() != 0){
+            films[i].liked = true
+            films[i].vote_count += 1;
+        }else{
+            films[i].liked = false
+        }
+    }
 
 
-    res.end(JSON.stringify(result))
+    res.end(JSON.stringify(films))
 });
+
+
+
+WebApp.connectHandlers.use(connectRoute(function (router) {
+
+    //Search
+    router.post('/api/search/:textInput', (req, res, next) => {
+
+        let content = req.params.textInput;
+        console.log(JSON.stringify(req.params.textInput))
+        let url = 'https://api.themoviedb.org/3/search/movie?api_key=8cce820f4a3f0e0254ff5512352682fe&language=fr-FR&page=1&include_adult=true&query='+content;
+        console.log(url)
+        let result = HTTP.call('GET', url, {})
+
+        res.end(JSON.stringify(result))
+    });
+
+    //Like
+    router.post('/api/like/:idMovie', (req, res, next) => {
+        let result = "already liked"
+        let id = req.params.idMovie;
+        console.log(Likes.find({'idFilm' : id}).fetch())
+        if(Likes.find({idFilm : id.toString()}).count() == 0 && id != undefined && id != null){
+            console.log("id", id)
+
+            let filmLiked = {
+                idFilm: id,
+                liked: true,
+            }
+
+            Likes.insert(filmLiked)
+            result = "liked"
+        }
+        res.end(result)
+    })
+}))
+
